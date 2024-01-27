e11579f731cabd32f6f1f3d63a1199a30aa3183e
MohitRanjan2433 <mohitranjan11082@gmail.com>
2024-01-28 00:51:16 +0530

    updqate al

--- src/pages/LoginForm.js
+++ src/pages/LoginForm.js
@@ -1,11 +1,36 @@
 import React from 'react'
+import './LoginForm.css';
 
 const loginForm = () => {
   return (
     <div className='wrapper'>
-      <div>
+      <form action=''>
         <h1>Login</h1>
-      </div>
+        <div className='input-box'>
+          <input 
+          type='email'
+          placeholder='Email'
+          value='' 
+          required/>
+        </div>
+        <div className='input-box'>
+          <input 
+          type='password'
+          placeholder='Password'
+          value='' 
+          required/>
+        </div>
+
+        <div className='remember-forgot'>
+          <labe><input type='checkbox' />Remember Me</labe>
+          <a href='#'>Forgot Password</a>
+        </div>
+        <button className='submit' type='submit'>Login</button>
+
+        <div className='register-link'>
+          <p>Dont have a account? <a href='#'>Register</a></p>
+        </div>
+      </form>
     </div>
   )
 }
